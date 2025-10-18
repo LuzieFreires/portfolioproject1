1d97a873489e1f8e1c7c560ef5424c6b89772557
sygny <143326656+sygny@users.noreply.github.com>
2025-10-18 23:29:26 +0800

    3rd commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// === SCROLL REVEAL ANIMATION ===
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
